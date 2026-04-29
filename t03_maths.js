5edb15ab6e12807a59ca2c67db4079fe2a7fe2f0
24236dw-eng <24236dw@hvhs.school.nz>
2026-04-29 02:49:25 +0000

    Apr 29, 2026, 2:49 AM

--- t03_maths.js
+++ t03_maths.js
@@ -8,14 +8,17 @@ let username="Diwakar";
 let year= 2026;
 let age= 15;
 let money= 10;
-
+let birthYear= 2010;
+let oldAge;
+oldAge= age + 10;
+birthYear= year + age;
 
 /****************************
 Main code 
 ****************************/
 console.log("Hi " + username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars")
 
-
+console.log("You were born in ")
 
 
 /****************************
