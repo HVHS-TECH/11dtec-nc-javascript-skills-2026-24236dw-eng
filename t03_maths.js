a5d4b5b7156f0395739413952638fc4c75b9e049
24236dw-eng <24236dw@hvhs.school.nz>
2026-04-29 03:06:46 +0000

    Apr 29, 2026, 3:06 AM

--- t03_maths.js
+++ t03_maths.js
@@ -12,16 +12,17 @@ let birthYear;
 let oldAge;
 oldAge= age + 10;
 birthYear= year - age;
-halfmnoe
+halfmoney= money / 2
 
 /****************************
 Main code 
 ****************************/
 console.log("Hi " + username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars")
 console.log("You were born in " + birthYear)
-console.log("In 10 years you will be " + oldAge + " years old ")
-console.log("You have" + money "dollars")
-console.log("You were born in " + birthYear)
+console.log("In 10 years you will be " + oldAge + " years old")
+console.log("You have " + money + " dollars")
+console.log("You spend half your money, now you have " + halfmoney + "dollars")
+console.log("You get $3" +" now you have "+ (halfmoney + 3) + " dollars ")
 
 
 
