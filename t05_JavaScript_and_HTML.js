566eb1e5cc68201cef15496a1254b90b953947f8
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-04 23:45:48 +0000

    May 4, 2026, 11:45 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -13,18 +13,18 @@ let oldAge;
 oldAge= age + 10;
 birthYear= year - age;
 halfmoney= money / 2
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 
 /****************************
 Main code 
 ****************************/
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 
 OUTPUT.innerHTML += "<p> Hi " + username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars</p>";
-OUTPUT.innerHTML += "<p>You were born in " + birthYear"</p>";
+OUTPUT.innerHTML += "<p>You were born in " + birthYear + "</p>";
 OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old</p>";
 OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
-OUTPUT.innerHTML += "<p>You spend half your money, now you have " + halfmoney + " dollars/p>";
+OUTPUT.innerHTML += "<p>You spend half your money, now you have " + halfmoney + " dollars</p>";
 OUTPUT.innerHTML += "<p>You get $3," +" now you have "+ (halfmoney + 3) + " dollars</p>";
 
 
