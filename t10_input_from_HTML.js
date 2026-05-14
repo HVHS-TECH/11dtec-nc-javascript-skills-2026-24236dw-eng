3b41aef91a84415f0b76fd72fdc53e14f4fbb2bd
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-14 21:39:05 +0000

    May 14, 2026, 9:39 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -19,3 +19,8 @@ const NAME_FIELD = document.getElementById("nameField");
 let userName = NAME_FIELD.value;
 OUTPUT.innerHTML ="<p>Your name is "+userName+"</p>";
 }
+function getFormInput(){
+const NAME_FIELD = document.getElementById("moneyField");
+let userName = NAME_FIELD.value;
+OUTPUT.innerHTML ="<p>Your name is "+userName+"</p>";
+}
