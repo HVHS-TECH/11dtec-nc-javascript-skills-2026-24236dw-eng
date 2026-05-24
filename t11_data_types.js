a14f58ee67f17a0d7673a9e5c896ef0b7fcadc84
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-24 22:56:54 +0000

    May 24, 2026, 10:56 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,21 @@
-/* Paste your code from the last task */
+console.log("t10_ijput_from_HTML.js");
+
+/****************************
+ Main Code
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML= "Hello world!<br>";
+/****************************
+ Functions
+ 
+ ****************************/
+function start() {
+  console.log("Button pressed!");
+
+  OUTPUT.innerHTML = "<p>You pressed the button!</p>";
+}
+function getFormInput(){
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+OUTPUT.innerHTML ="<p>Your name is "+userName+"</p>";
+}
