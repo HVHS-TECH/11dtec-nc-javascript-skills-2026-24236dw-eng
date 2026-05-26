6477e43db12ff272d0cc41d501ce5a90aaaeca91
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-26 10:47:12 +0000

    May 26, 2026, 10:47 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -4,7 +4,7 @@ console.log("t10_ijput_from_HTML.js");
  Main Code
 ****************************/
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML= "Hello world!<br>";
+OUTPUT.innerHTML = "Hello world!<br>";
 /****************************
  Functions
  
@@ -14,9 +14,22 @@ function start() {
 
   OUTPUT.innerHTML = "<p>You pressed the button!</p>";
 }
-function getFormInput(){
-const NAME_FIELD = document.getElementById("nameField");
-let userName = NAME_FIELD.value;
-OUTPUT.innerHTML ="<p>Your name is "+userName+"</p>";
+function getFormInput() {
+  const NAME_FIELD = document.getElementById("nameField");
+  const MONEY_FIELD = document.getElementById("moneyField");
+  const AGE_FIELD = document.getElementById("ageField");
+  const BIRTHYEAR_FIELD = document.getElementById("BirthyearField");
+
+  let userName = NAME_FIELD.value;
+  let userMoney = MONEY_FIELD.value;
+  let userAge = AGE_FIELD.value;
+  let userBirthYear = BIRTHYEAR_FIELD.value;
+
+  OUTPUT.innerHTML =
+  "<p>Your name is " + userName + "</p>";
+  "<p>Your money is $" + userMoney + "</p>";
+  "<p>Your age is " + userAge + "</p>";
+  "<p>Your birth year is " + userBirthYear + "</p>";
+
 }
 
