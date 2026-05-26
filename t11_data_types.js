5e03b4e3813aa4b0194606d36d83c4ed8d42df25
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-26 10:55:26 +0000

    May 26, 2026, 10:55 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,14 +1,10 @@
 console.log("t11_ijput_from_HTML.js");
-/****************************
- Variables
-****************************/
-let userAge = Number(AGE_FIELD.value);
 
 /****************************
  Main Code
 ****************************/
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML= "Hello world!<br>";
+OUTPUT.innerHTML = "Hello world!<br>";
 /****************************
  Functions
  
@@ -18,10 +14,23 @@ function start() {
 
   OUTPUT.innerHTML = "<p>You pressed the button!</p>";
 }
-function getFormInput(){
-const AGE_FIELD_FIELD = document.getElementById("ageField");
-let userName = AGE_FIELD.value;
-OUTPUT.innerHTML ="<p>Your name is "+userName+"</p>";
+function getFormInput() {
+  const NAME_FIELD = document.getElementById("nameField");
+  const MONEY_FIELD = document.getElementById("moneyField");
+  const AGE_FIELD = document.getElementById("ageField");
+  const BIRTHYEAR_FIELD = document.getElementById("BirthyearField");
+
+  let userName = NAME_FIELD.value;
+  let userMoney = MONEY_FIELD.value;
+  let userAge = AGE_FIELD.value;
+  let userBirthYear = BIRTHYEAR_FIELD.value;
+
+  OUTPUT.innerHTML =
+  "<p>Your name is " + userName + "</p>" +
+  "<p>Your money is $" + userMoney + "</p>" +
+  "<p>Your age is " + userAge + "</p>" +
+  "<p>Your birth year is " + userBirthYear + "</p>";
+
 }
 
 
