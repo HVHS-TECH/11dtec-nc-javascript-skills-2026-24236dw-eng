8f5273b298b484c40f3b16cbbd8ecc6e1f3276a0
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-27 02:38:51 +0000

    May 27, 2026, 2:38 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,34 @@
-/* Paste your code from the last task */
+console.log("t11_ijput_from_HTML.js");
+
+/****************************
+ Main Code
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "Hello world!<br>";
+/****************************
+ Functions
+ 
+ ****************************/
+function start() {
+  console.log("Button pressed!");
+
+  OUTPUT.innerHTML = "<p>You pressed the button!</p>";
+}
+function getFormInput() {
+  const NAME_FIELD = document.getElementById("nameField");
+  const MONEY_FIELD = document.getElementById("moneyField");
+  const AGE_FIELD = document.getElementById("ageField");
+  const BIRTHYEAR_FIELD = document.getElementById("BirthyearField");
+
+  let userName = NAME_FIELD.value;
+  let userMoney = MONEY_FIELD.value;
+  let userAge = AGE_FIELD.value;
+  let userBirthYear = BIRTHYEAR_FIELD.value;
+
+  OUTPUT.innerHTML =
+  "<p>Your name is " + userName + "</p>" +
+  "<p>Your money is $" + userMoney + "</p>" +
+  "<p>Your age is " + userAge + "</p>" +
+  "<p>Your birth year is " + userBirthYear + "</p>";
+
+}
