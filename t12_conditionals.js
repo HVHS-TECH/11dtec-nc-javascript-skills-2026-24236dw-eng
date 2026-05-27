e2f7bf89746c979dda6476680be3f4c4ec0c23a6
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-27 10:45:19 +0000

    May 27, 2026, 10:45 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -53,11 +53,11 @@ spaceForJavaScriptOutput.innerHTML
 console.log("Definitely to young") 
 }
 
-if (age > 70){
+if (age >=70){
 console.log("Might be to old")
 }
 
-if (age = 70){
+if (age == 70){
 console.log("Just right")
 }
 
