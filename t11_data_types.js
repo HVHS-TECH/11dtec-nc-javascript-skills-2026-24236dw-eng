e09c78fd371e4235018a006b430bdc013182dcc8
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-29 07:41:16 +0000

    May 29, 2026, 7:41 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,46 +1,53 @@
-console.log("t11_ijput_from_HTML.js");
-
 /****************************
- Main Code
+Input from HTML
 ****************************/
+
+console.log("Running t11_data_types.js")
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = "Hello world!<br>";
+OUTPUT.innerHTML += "<h2>added by javascrript</h2>";
+
+//variables
+let name = "Diwakar";
+let pocketMoney = 2;
+let age = 16;
+let year = 2026;
+var yearBorn = year-age;
+var tenYears = age+10;
+var halfMoney = pocketMoney/2;
+var plusThree = pocketMoney/2+3;
 /****************************
- Functions
- 
- ****************************/
-
-function start() {
-  console.log("Button pressed!");
-
-  OUTPUT.innerHTML = "<p>You pressed the button!</p>";
+main code
+****************************/
+function start(){
+OUTPUT.innerHTML += "<p>Hi "+ name;
+OUTPUT.innerHTML += "<p>as of " + year + " you are " + age + " years old";
+OUTPUT.innerHTML += "<p>you were born in " + yearBorn;
+OUTPUT.innerHTML += "<p>In 10 years, you will be " + tenYears;
+OUTPUT.innerHTML += "<p>you have " + pocketMoney + " dollars.";
+OUTPUT.innerHTML += "<p>you spend half of your money, now you have " + halfMoney + " dollars";
+OUTPUT.innerHTML += "<p>then you get $3, you now have " + plusThree + " dollars" ;
+welcome()
+displayProduct("<p>chocolate: ", "$4.00");
+displayProduct("<p>chips: ", "$3.00");
+displayProduct("<p>drink: ", "$2.50");
 }
-function getFormInput() {
-  const USERNAGE_FIELD = document.getElementById("UserAgeField");
-  const POCKETMONEY_FIELD = document.getElementById("PocketMoneyField");
 
+/****************************
+functions
+****************************/
 
-  let userName = USERAGE_FIELD.value;
-  let userMoney = POCKETMONEY_FIELD.value;
-
-}
-function welcome () {
-OUTPUT.innerHTML += "<p>Welcome to my shop</p>"
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
 
 }
-welcome ();
-function displayProduct(_name, _price) {
-OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>";
+
+
+function welcome(){
+    OUTPUT.innerHTML += "<p>welcome to the shop:";
 }
-displayProduct("Chocolate bar", 4);
-displayProduct("Chips", 3);
-displayProduct("Drink", 2.50);
 
-
-
-
-   OUTPUT.innerHTML =
-  "<p>Your name is " + userName + "</p>" +
-  "<p>Your money is $" + userMoney + "</p>" +
-  "<p>Your age is " + userAge + "</p>" +
-  "<p>Your birth year is " + userBirthYear + "</p>";
+function displayProduct(_name, _price){
+    OUTPUT.innerHTML += _name + _price
+}
