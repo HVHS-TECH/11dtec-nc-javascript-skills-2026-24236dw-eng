5106207dd35ecf15134a66f883cf240718281e2b
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-30 07:12:55 +0000

    May 30, 2026, 7:12 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,5 +1,6 @@
+
 /****************************
-Input from HTML
+data types
 ****************************/
 
 console.log("Running t11_data_types.js")
@@ -7,23 +8,31 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<h2>added by javascrript</h2>";
 
 //variables
-let name = "Diwakar";
-let pocketMoney = 2;
-let age = 16;
-let year = 2026;
-var yearBorn = year-age;
-var tenYears = age+10;
-var halfMoney = pocketMoney/2;
-var plusThree = pocketMoney/2+3;
+
+
 /****************************
 main code
 ****************************/
 function start(){
-OUTPUT.innerHTML += "<p>Hi "+ name;
-OUTPUT.innerHTML += "<p>as of " + year + " you are " + age + " years old";
+const NAME_FIELD = document.getElementById("nameField");
+ let Username = NAME_FIELD.value;
+const AGE_FIELD = document.getElementById("ageField");
+ let Userage = Number(AGE_FIELD.value);
+ const MONEY_FIELD = document.getElementById("moneyField");
+let UserpocketMoney = Number(MONEY_FIELD.value);
+let name = "Diwakar";
+let pocketMoney = 2;
+let age = 16;
+let year = 2026;
+var yearBorn = year-Userage;
+var tenYears = Userage+10;
+var halfMoney = UserpocketMoney/2;
+var plusThree = UserpocketMoney/2+3;
+OUTPUT.innerHTML += "<p>Hi "+ Username;
+OUTPUT.innerHTML += "<p>as of " + year + " you are " + Userage + " years old";
 OUTPUT.innerHTML += "<p>you were born in " + yearBorn;
 OUTPUT.innerHTML += "<p>In 10 years, you will be " + tenYears;
-OUTPUT.innerHTML += "<p>you have " + pocketMoney + " dollars.";
+OUTPUT.innerHTML += "<p>you have " + UserpocketMoney + " dollars.";
 OUTPUT.innerHTML += "<p>you spend half of your money, now you have " + halfMoney + " dollars";
 OUTPUT.innerHTML += "<p>then you get $3, you now have " + plusThree + " dollars" ;
 welcome()
@@ -35,17 +44,8 @@ displayProduct("<p>drink: ", "$2.50");
 /****************************
 functions
 ****************************/
-
-function getFormInput(){
-    const USERAGE_FIELD = document.getElementById("userageField");
-    let userAge = USERAGE_FIELD.value;
-    OUTPUT.innerHTML = "<p>Your age is "+userAge+"</p>";
-
-}
-
-
 function welcome(){
-    OUTPUT.innerHTML += "<p>welcome to the shop<p>";
+    OUTPUT.innerHTML += "<p>welcome to the shop:";
 }
 
 function displayProduct(_name, _price){
