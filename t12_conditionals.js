4b91f9d3982cba4664feddb55ce1796f1274d998
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-31 04:36:59 +0000

    May 31, 2026, 4:36 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,12 +1,13 @@
 /****************************
-Conditionals
+data types
 ****************************/
 
-console.log("Running t11_conditionals.js")
+console.log("Running t12_conditionals.js")
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<h2>added by javascrript</h2>";
 
 //variables
+
 
 /****************************
 main code
@@ -18,35 +19,34 @@ const AGE_FIELD = document.getElementById("ageField");
  let Userage = Number(AGE_FIELD.value);
  const MONEY_FIELD = document.getElementById("moneyField");
 let UserpocketMoney = Number(MONEY_FIELD.value);
-let name = "archie";
+let name = "Diwakar";
 let pocketMoney = 2;
 let age = 16;
 let year = 2026;
 var yearBorn = year-Userage;
 var tenYears = Userage+10;
 var halfMoney = UserpocketMoney/2;
-var pocketMoney2 = UserpocketMoney/2+3;
+var plusThree = UserpocketMoney/2+3;
 OUTPUT.innerHTML += "<p>Hi "+ Username;
 OUTPUT.innerHTML += "<p>as of " + year + " you are " + Userage + " years old";
 OUTPUT.innerHTML += "<p>you were born in " + yearBorn;
 OUTPUT.innerHTML += "<p>In 10 years, you will be " + tenYears;
 OUTPUT.innerHTML += "<p>you have " + UserpocketMoney + " dollars.";
 OUTPUT.innerHTML += "<p>you spend half of your money, now you have " + halfMoney + " dollars";
-OUTPUT.innerHTML += "<p>then you get $3, you now have " + pocketMoney2 + " dollars" ;
-
+OUTPUT.innerHTML += "<p>then you get $3, you now have " + plusThree + " dollars" ;
 welcome()
 displayProduct("<p>chocolate: ", "$4.00");
 displayProduct("<p>chips: ", "$3.00");
 displayProduct("<p>drink: ", "$2.50");
-    if (pocketMoney2 >= 4){
-        OUTPUT.innerHTML += "<p>you have enough to buy a chocolate bar";
-    }
-    else{
-        OUTPUT.innerHTML += "<p>you do not have enough to buy a chocolate bar"
-    }
-
 }
 
+if (userMoney < chocolateCost) {
+OUTPUT.innerHTML += "<p>A chocolate bar costs $4<br> Sorry you can not afford a chcocolate bar</p>";
+}
+else (userMoney> chocolateCost) 
+console.log ("you can buy a chocolate")
+
+    
 /****************************
 functions
 ****************************/
