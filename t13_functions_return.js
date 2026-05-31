f38c7b428e7859b5fa549f17314dee4528641db1
24236dw-eng <24236dw@hvhs.school.nz>
2026-05-31 05:31:49 +0000

    May 31, 2026, 5:31 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -65,7 +65,7 @@ pay = pay + 10;
 return pay;
 
 OUTPUT.innerHTML += ":<br>"
-OUTPUT.innerHTML += "chocolate: $"+mondayPay+"<br>";
+OUTPUT.innerHTML += "chocolate: $"++"<br>";
 }
 
 let chocolatePay = calculatePay (4);
