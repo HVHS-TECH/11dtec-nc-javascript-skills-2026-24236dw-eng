5c918b1a4665abdfe597023e493e8169e24630ca
24236dw-eng <24236dw@hvhs.school.nz>
2026-06-01 05:16:34 +0000

    Jun 1, 2026, 5:16 AM

--- t02_variables.js
+++ t02_variables.js
@@ -8,13 +8,13 @@ let username="Diwakar";
 let year= 2026;
 let age= 16;
 let money= 10;
-
+let season= 4;
 
 /****************************
 Main code 
 ****************************/
 console.log("Hi " + username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars")
-
+console.log("There are " + season + " seasons in a year")
 
 
 
